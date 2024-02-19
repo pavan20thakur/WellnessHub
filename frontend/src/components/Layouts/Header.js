fb55cc99667da467462e3ef2060e3821091ab58a
Pavan Thakur <pt20112003@gmail.com>
2024-02-19 17:08:01 +0530

    new changes

--- frontend/src/components/Layouts/Header.js
+++ frontend/src/components/Layouts/Header.js
@@ -32,7 +32,7 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link to="/" className="navbar-brand">
-              MINI-PROJECT App
+              Syrus App
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
@@ -40,7 +40,11 @@ const Header = () => {
                   Home
                 </NavLink>
               </li>
-              {
+              <li className="nav-item">
+                <NavLink to="/category" className="nav-link ">
+                 About
+                </NavLink>
+              </li>{
               (!auth.user)? (<>
                 <li className="nav-item">
                 <NavLink to="/register" className="nav-link">
@@ -52,16 +56,40 @@ const Header = () => {
                   Login
                 </NavLink>
               </li>
-              </>) : (<>
-                <li className="nav-item">
-                <NavLink onClick={handleLogout} to="/login" className="nav-link">
-                  Logout
-                </NavLink>
-              </li>
-              </>)}
+              </>) :(
+                <>
+                  <li className="nav-item dropdown">
+                    <NavLink
+                      className="nav-link dropdown-toggle"
+                      href="#"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      {auth?.user?.name}
+                    </NavLink>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <NavLink to={`/dashboard/${auth?.user?.role === 1 ? "admin" : "user"}`} className="dropdown-item">
+                          Dashboard
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          onClick={handleLogout}
+                          to="/login"
+                          className="dropdown-item"
+                        >
+                          Logout
+                        </NavLink>
+                      </li>
+                    </ul>
+                  </li>
+                </>
+              )}
               <li className="nav-item">
                 <NavLink to="/cart" className="nav-link">
-                  Profile
+                  Contact Us
                 </NavLink>
               </li>
             </ul>

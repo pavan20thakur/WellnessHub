968aa8f34132193e014dac8439867a98c5ef34f4
Pavan Thakur <pt20112003@gmail.com>
2024-02-20 09:49:58 +0530

    newcommit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import PageNotFound from './pages/PageNotFound';
 import Register from './pages/auth/Register';
 import Login from './pages/auth/Login';
 import HomePage from './pages/HomePage';
+import Dashboard from './pages/user/Dashboard';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path='/' element={<HomePage />} />
       <Route path='/register' element={<Register />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/dashboard' element={<Dashboard />} />
       <Route path='*' element={<PageNotFound />} />
     </Routes>
     </>

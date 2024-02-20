7aa0c36b793b7864dd3bf85fdd240f621607e459
Pavan Thakur <pt20112003@gmail.com>
2024-02-20 21:07:47 +0530

    User Model Fixed

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,22 +1,18 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const { ObjectId } = Schema
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-
-    name: {
+    name:{
         type: String,
+        required: true
     },
-    email: {
+    email:{
         type: String,
         required: true,
         unique: true
     },
-    picture: {
-        type: String
-    },
-    authCode: {
-        type: String
+    password:{
+        type: String,
+        required: true
     },
     waterGoal: {
         type: Number,
@@ -30,10 +26,10 @@ const userSchema = new mongoose.Schema({
         type: Number,
         default: 3600
     },
- 
+},
+{
+    timestamps: true
+})
 
-}, { timestamps: true })
-
-module.exports = mongoose.model("User", userSchema)
-
-
+const userdb = mongoose.model("users", userSchema);
+module.exports = userdb;

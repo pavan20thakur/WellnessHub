2c168dbd3c0b87c8a609245b2fd47da064c40dc0
Ilham-Syed <ilhamsyed1310@gmail.com>
2024-02-21 10:39:53 +0530

    Questionnaire updated

--- frontend/src/components/DashboardComponents/ScorePopup.jsx
+++ frontend/src/components/DashboardComponents/ScorePopup.jsx
@@ -5,8 +5,18 @@ import React from "react";
 const ScorePopup = ({ score, onClose }) => {
   let message = "";
 
-  if (score < 12) {
-    message = "You seem Anxious !! Consider visiting our Relaxation Section";
+  if (score < 9) {
+    message = (
+      <>
+        <p>Your mental health needs attention!!</p>
+        
+        <p>
+            Consider Surfing through our website
+        </p>
+      </>
+    );
+  } else if (score >= 9 && score < 14) {
+    message = "Your mental health is in a moderate range. Consider self-care practices.";
   } else {
     message = "Your mental health is in good shape. Keep it up!";
   }
@@ -16,7 +26,7 @@ const ScorePopup = ({ score, onClose }) => {
       <div className="score-container bg-white p-8 rounded-md shadow-md">
         <h2 className="text-2xl font-bold mb-4">Your Score</h2>
         <p>Your score is: {score}</p>
-        <p>{message}</p>
+        {message}
         <button
           onClick={onClose}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4"
